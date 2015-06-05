8ea73786831783c62132911835643aace126f665
ychuang <ychuang@thoughtworks.com>
2015-06-05 09:30:21 +0800

    add active class when click nav

--- js/controller.js
+++ js/controller.js
@@ -1,11 +1,12 @@
 var app = angular.module('app',[]);
 
 app.controller('navController', function($scope){
-     
-  $scope.lititle =  $('li a:first').attr('title');
-
+    $scope.lititle =  $('header li a:first').attr('title');
+    $('header li:first').addClass('active');
 
       $scope.change = function($event){
+        $('header li').removeClass('active');
+        $($event.target).parent().addClass('active');
         $scope.lititle = $($event.target).attr('title');
       }
 });
